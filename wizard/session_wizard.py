88670b9afd72b8328233961183bf0da313b4fad1
douniabennoune <56886208+douniabennoune@users.noreply.github.com>
2021-12-21 12:08:15 +0100

    update session_wizard.py

--- wizard/session_wizard.py
+++ wizard/session_wizard.py
@@ -1,10 +1,16 @@
 from odoo import models, fields, api
-from odoo.exceptions import ValidationError
-from datetime import timedelta, time
 
 
 class SessionWizard(models.TransientModel):
     _name = "session.wizard"
 
+    def _default_sessions(self):
+        return self.env['session'].browse(self._context.get('active_ids'))
 
+    session_ids = fields.Many2many("session", required=True, string="Session", default=_default_sessions)
+    attendees = fields.Many2many("res.partner")
 
+    def subscribe(self):
+        for session in self.session_ids:
+            session.attendees |= self.attendees
+        return {}
